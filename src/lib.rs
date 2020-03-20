e60734f8a6511658a507762233d41c793e081edb
Frank Denis <github@pureftpd.org>
2020-03-20 15:04:17 +0100

    Clippy options

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@
 //!
 //! See <https://www.cryptolux.org/index.php/SPARX> for more information about SPARX.
 
+#![allow(clippy::erasing_op)]
+#![allow(clippy::identity_op)]
+
 #![cfg_attr(not(test), no_std)]
 
 pub mod sparx128;
